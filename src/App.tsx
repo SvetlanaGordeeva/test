5529abfea819dcc9392d029b2e542d57ad770214
SvetlanaGordeeva <gordeeva.sveetlana@yandex.ru>
2023-08-02 17:07:05 +0300

    Commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+              </p>
+        Lorem 
         <a
           className="App-link"
           href="https://reactjs.org"
